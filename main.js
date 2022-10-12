22d73e38a3dbe0da5dcca9cc7f96d8bc21b06c0d
louisep1 <89385533+louisep1@users.noreply.github.com>
2022-10-12 20:43:32 +0900

    cleaned comments in .js

--- main.js
+++ main.js
@@ -2,21 +2,13 @@
 const fadeSection = document.querySelectorAll('.fade-on-scroll')
 
 window.addEventListener('scroll', () => {
-  // console.log(window.pageYOffset)
   fadeSection.forEach((section, i) => {
-    // if (section.parentElement.getBoundingClientRect().top <= 0) {
     if (section.getBoundingClientRect().top <= 300) {
       fadeSection[i].classList.add('appear')
       console.log(fadeSection[i].classList)
     }
-    // console.log(section.getBoundingClientRect().top)
-    // console.log(window.pageYOffset)
-    // console.log(section.offsetHeight)
   })
 })
-
-// console.log(doc.scrollTop)
-// console.log(doc.clientTop)
 
 // Expandable/drop down sections:
 const click = document.querySelectorAll('#click')
